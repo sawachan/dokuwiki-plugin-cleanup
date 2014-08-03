f3bcd1002764540a76bfacaf237fa878c912172f
sawachan <chuno@live.jp>
2014-08-03 19:51:39 +0900

    Create lang.php

---
+++ lang/ja/lang.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ */
+ 
+$lang['menu']                  = 'ファイルの整理';
+$lang['sum']                   = '%d ファイル %s が削除されます。';
+$lang['preview']               = '対象ファイルのプレビュー';
+$lang['execute']               = '対象ファイルの削除';
+$lang['done']                  = 'ファイルは削除されました。';
