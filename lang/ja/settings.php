d13c967574209ddcb99d6948521d8528bc705455
sawachan <chuno@live.jp>
2014-08-03 19:53:09 +0900

    Create settings.php

---
+++ lang/ja/settings.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ */
+
+$lang['runautomatically']      = 'ファイル整理処理を自動的に実行する？';
+$lang['cacheage']              = 'この時間（秒）経過したキャッシュファイルを削除します。';
+$lang['atticage']              = 'この時間（秒）経過したページの改訂履歴を削除します。';
+$lang['atticnoexonly']         = '現存しないページの改訂履歴を削除します。';
+$lang['mediaatticage']         = 'この時間（秒）経過したメディアの改訂履歴を削除します。';
+$lang['mediaatticnoexonly']    = '現存しないメディアの改訂履歴を削除します。';
+$lang['metaage']               = 'この時間（秒）経過した存在しないページのメタデータを削除します。';
+$lang['mediametaage']          = 'この時間（秒）経過した存在しないメディアのメタデータを削除します。';
+$lang['lockage']               = 'この時間（秒）経過した古いロックファイルを削除します。';
